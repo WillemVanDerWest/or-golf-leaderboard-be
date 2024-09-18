d00a2df3ac87b6c30b836784ad7612dc0b9fe234
Willem Van der westhuizen <willem04011999@gmail.com>
2024-09-18 19:58:41 +0200

    Start

---
+++ src/main/java/za/co/theor/GolfLeaderBoard/GolfLeaderBoardApplication.java
@@ -0,0 +1,13 @@
+package za.co.theor.GolfLeaderBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfLeaderBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfLeaderBoardApplication.class, args);
+	}
+
+}
