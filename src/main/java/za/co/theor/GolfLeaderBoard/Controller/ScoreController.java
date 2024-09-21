8ac6ac5b8ffc418bda471bd5b49d2abc219ad082
Willem Van der westhuizen <willem04011999@gmail.com>
2024-09-21 11:45:02 +0200

    read through table and send back names with handicaps

--- src/main/java/za/co/theor/GolfLeaderBoard/Controller/ScoreController.java
+++ src/main/java/za/co/theor/GolfLeaderBoard/Controller/ScoreController.java
@@ -1,16 +1,18 @@
 package za.co.theor.GolfLeaderBoard.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import za.co.theor.GolfLeaderBoard.Entity.Player;
 import za.co.theor.GolfLeaderBoard.Entity.RoundScore;
 import za.co.theor.GolfLeaderBoard.Repository.PlayerRepository;
 import za.co.theor.GolfLeaderBoard.Repository.RoundScoreRepository;
 import za.co.theor.GolfLeaderBoard.model.CaptureScoreRequest;
+import za.co.theor.GolfLeaderBoard.model.PlayerNameAndScoreListType;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/scoreController")
@@ -91,6 +93,71 @@ public class ScoreController {
         avgScore = totalScore / roundScores.size();
 
         return name + "'s score: " + avgScore;
+    }
+
+
+    @GetMapping("/{name}")
+    public Object calculatePlayerHandicap(@PathVariable String name){
+//
+//        Player player = playerRepository.findByName(name);
+//
+//        if (player.getName() == null) {
+//            return String.valueOf(new ResponseEntity<>("Could not find that name", HttpStatus.NOT_FOUND));
+//        }
+//
+//        List<RoundScore> playerScores = player.getScores();
+//        List<Double> sixMonthScores = new ArrayList<>();
+//        double avgScore = 0.0;
+//        double totalScore = 0.0;
+//
+//        LocalDateTime sixMonthsAgoDate = LocalDateTime.now().minusMonths(6);
+//
+//        for (int i = 0; i < playerScores.size(); i++) {
+//            var isPlayerScoreValid = playerScores.get(i).getDate().isAfter(sixMonthsAgoDate);
+//            var validPlayerScoreValue = playerScores.get(i).getScore();
+//
+//            if (isPlayerScoreValid) {
+//                sixMonthScores.add(validPlayerScoreValue);
+//            }
+//        }
+//
+//        for (int i = 0; i < sixMonthScores.size(); i++) {
+//           totalScore= totalScore + sixMonthScores.get(i);
+//        }
+//        avgScore= totalScore/playerScores.size();
+
+
+        //find all the players in the list
+        //generate the avgScore of all the players
+        //return a string of each player name and their average score
+
+        var getAllPlayersData = playerRepository.findAll();
+        var listOfPlayers = new ArrayList<>();
+        var currentPlayer = new PlayerNameAndScoreListType();
+
+        for (int i = 0; i < getAllPlayersData.size(); i++) {
+
+            var totalScoresAvailableForPlayer = getAllPlayersData.get(i).getScores().size();
+            var currentPlayerTotalScore = 0.0;
+            var currentPlayerName = getAllPlayersData.get(i).getName();
+
+            for (int j = 0; j < totalScoresAvailableForPlayer; j++) {
+                currentPlayerTotalScore = currentPlayerTotalScore + getAllPlayersData.get(i).getScores().get(j).getScore();
+            }
+
+            var currentPlayerHandicap = currentPlayerTotalScore/ totalScoresAvailableForPlayer;
+            currentPlayer.setHandicap(currentPlayerHandicap);
+            currentPlayer.setName(currentPlayerName);
+
+            listOfPlayers.add(i, currentPlayer);
+        }
+
+
+
+
+
+        return listOfPlayers;
+
     }
 }
 
---
+++ src/main/java/za/co/theor/GolfLeaderBoard/model/PlayerNameAndScoreListType.java
@@ -0,0 +1,9 @@
+package za.co.theor.GolfLeaderBoard.model;
+
+import lombok.Data;
+
+@Data
+public class PlayerNameAndScoreListType {
+    private String name;
+    private double handicap;
+}
